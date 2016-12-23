2767885381c523bcdf2e4ee8b6ff29335b641625
Ryuichi Ueda <ryuichiueda@gmail.com>
2016-12-23 18:12:51 +0900

    Change a node name

--- scripts/face_to_face1.py
+++ scripts/face_to_face1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+#encoding: utf8
 import rospy, cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-class FaceDetectControl():
+class FaceToFace():
     def __init__(self):
         sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.get_image)
         self.bridge = CvBridge()
@@ -12,21 +13,20 @@ class FaceDetectControl():
     def get_image(self,img):
     	try:
             self.image_org = self.bridge.imgmsg_to_cv2(img, "bgr8")
-    	except CvBridgeError as e:
+        except CvBridgeError as e:
             rospy.logerr(e)
 
     def control(self):
         if self.image_org is None:
             return None
 
-        return self.image_org.shape
+        return id(self.image_org), type(self.image_org), self.image_org.shape
 
 if __name__ == '__main__':
     rospy.init_node('face_detect')
-    fd = FaceDetectControl()
+    f = FaceToFace()
 
     rate = rospy.Rate(5)
     while not rospy.is_shutdown():
-        rospy.loginfo(fd.control())
+        rospy.loginfo(f.control())
         rate.sleep()
-
