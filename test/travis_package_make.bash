0dce74544fd0ee92cc3e0810f47714fb4bb32e37
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-01-03 08:39:11 +0900

    Try complicated install

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -1,10 +1,49 @@
 #!/bin/bash -xve
 
-sudo apt-get install libopencv-dev
+sudo apt-get install build-essential cmake git pkg-config
+sudo apt-get install libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev
+sudo apt-get install libgtk2.0-dev
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install libatlas-base-dev gfortran
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install virtualenv virtualenvwrapper
+sudo rm -rf ~/.cache/pip
+export WORKON_HOME=$HOME/.virtualenvs
+source /usr/local/bin/virtualenvwrapper.sh
+source ~/.bashrc
+mkvirtualenv cv
+sudo apt-get install python2.7-dev
+pip install numpy
+cd ~
+git clone https://github.com/Itseez/opencv.git
+cd opencv
+git checkout 3.0.0
+cd ~
+git clone https://github.com/Itseez/opencv_contrib.git
+cd opencv_contrib
+git checkout 3.0.0
+cd ~/opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+	-D CMAKE_INSTALL_PREFIX=/usr/local \
+	-D INSTALL_C_EXAMPLES=ON \
+	-D INSTALL_PYTHON_EXAMPLES=ON \
+	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+	-D BUILD_EXAMPLES=ON ..
+
+make
+
+sudo make install
+sudo ldconfig
+
+cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
+ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
+
+#sudo apt-get install libopencv-dev
 sudo apt-get install ros-indigo-cv-bridge
 sudo apt-get install ros-indigo-cv-camera
-pip install --upgrade pip
-pip install python-opencv
 
 #sync and make
 rsync -av ./ ~/catkin_ws/src/pimouse_vision_control/
