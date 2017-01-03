18780c9d537094812d4c342d51fd831ef1f813e9
Ryuichi Ueda <ryuichiueda@gmail.com>
2017-01-03 11:47:38 +0900

    add ldconf

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -3,6 +3,9 @@
 sudo apt-get update
 sudo apt-get install libopencv-dev python-opencv
 
+sudo /bin/bash -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+
 #sudo apt-get install libopencv-dev
 sudo apt-get install ros-indigo-cv-bridge
 sudo apt-get install ros-indigo-cv-camera
